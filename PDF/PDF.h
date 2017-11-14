0fbc4b873e29e011b8cf543ecb1039d1cf0897b5
Vui Nguyen <vui_engineer@yahoo.com>
2017-11-14 15:33:06 -0700

    initial framework & project infrastructure

---
+++ PDF/PDF.h
@@ -0,0 +1,19 @@
+//
+//  PDF.h
+//  PDF
+//
+//  Created by Vui Nguyen on 11/14/17.
+//  Copyright © 2017 Minitex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDF.
+FOUNDATION_EXPORT double PDFVersionNumber;
+
+//! Project version string for PDF.
+FOUNDATION_EXPORT const unsigned char PDFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDF/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# PDF-iOS
+
+## Trying the Example App
+
+1. Run `./swiftlint-setup.sh` to install a local copy of [SwiftLint](https://github.com/realm/SwiftLint).
+2. Open `PDF.xcodeproj`.
+3. Ensure the `PDFExample` target is selected.
+4. Build and run as normal.
