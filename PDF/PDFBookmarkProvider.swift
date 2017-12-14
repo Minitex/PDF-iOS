8de532ef1a3386fdcd04a69af70535ec988a3ca9
Vui Nguyen <vui_engineer@yahoo.com>
2017-12-14 09:09:38 -0700

    add and remove bookmarks

--- PDF/PDFBookmarkProvider.swift
+++ PDF/PDFBookmarkProvider.swift
@@ -18,12 +18,29 @@ class PDFBookmarkProvider: NSObject, PSPDFBookmarkProvider {
 
   func add(_ bookmark: PSPDFBookmark) -> Bool {
     print("PDFBookmarkProvider, add called")
+    print("value of bookmark: \(bookmark)")
+
+    let index = bookmarks.index(of: bookmark)
+    if index == nil {
+      bookmarks.append(bookmark)
+    }
+    else {
+      bookmarks[index!] = bookmark
+    }
     return true
   }
 
   func remove(_ bookmark: PSPDFBookmark) -> Bool {
     print("PDFBookmarkProvider, remove called")
-    return true
+    print("value of bookmark: \(bookmark)")
+
+    let index = bookmarks.index(of: bookmark)
+    if bookmarks.contains(bookmark) {
+      bookmarks.remove(at: index!)
+      return true
+    } else {
+      return false
+    }
   }
 
   func save() {
