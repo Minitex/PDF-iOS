ee027804f3e6ab0b0f9c6b74d48bb7c5558e2951
Vui Nguyen <vui_engineer@yahoo.com>
2017-12-21 16:16:34 -0700

    add first UITest for bookmarks

--- PDFExampleUITests/PDFExampleUITests.swift
+++ PDFExampleUITests/PDFExampleUITests.swift
@@ -9,28 +9,34 @@
 import XCTest
 
 class PDFExampleUITests: XCTestCase {
-        
-    override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  var app: XCUIApplication!
+
+  override func setUp() {
+    super.setUp()
+
+    continueAfterFailure = false
+
+    app = XCUIApplication()
+    app.launch()
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testExample() {
+    app.buttons["Read Financial Accounting"].tap()
+
+    let pdfPdfviewNavigationBar = app.navigationBars["PDF.PDFView"]
+    let bookmarksButton = pdfPdfviewNavigationBar.buttons["Bookmarks"]
+    bookmarksButton.swipeLeft()
+
+    bookmarksButton.tap()
+    pdfPdfviewNavigationBar.buttons["Thumbnails"].tap()
+    app/*@START_MENU_TOKEN@*/.collectionViews["Thumbnail Collection"].cells["Page 380,  Bookmarked"]/*[[".otherElements[\"PDF View\"].collectionViews[\"Thumbnail Collection\"]",".cells[\"Page 380,  Bookmarked\"]",".cells[\"Page 380\"]",".collectionViews[\"Thumbnail Collection\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,1]]@END_MENU_TOKEN@*/.tap()
+    bookmarksButton.tap()
+  }
     
 }
