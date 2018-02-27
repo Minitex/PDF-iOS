699c1045637502388c6a3a0f028b1c73471a79a7
Vui Nguyen <vui_engineer@yahoo.com>
2018-02-27 11:12:37 -0700

    test importing changed last page read

--- PDF/Mock/MockData.swift
+++ PDF/Mock/MockData.swift
@@ -8,15 +8,98 @@
 
 import Foundation
 
-class MockData {
-  static let lastPageRead: UInt = 0
-  static let bookmarkPages: [UInt] = []
-  static let annotationsData: [Data] = []
+class MockData: MockPDFViewControllerDelegateDelegate {
+  var books: [Book]?
 
-  static func documentURL() -> URL {
-    let documentName = "DataModeling"
-    //let fileURL = Bundle.main.url(forResource: documentName, withExtension: "pdf")!
-    let fileURL = Bundle(identifier: "edu.umn.minitex.simplye.PDF")?.url(forResource: documentName, withExtension: "pdf")
+  // current book
+  var book: Book?
+  static var documentDirectoryURL: URL {
+    return try! FileManager.default.url(
+      for: .documentDirectory,
+      in: .userDomainMask,
+      appropriateFor: nil,
+      create: false
+    )
+  }
+
+  let booksPlistURL: URL = URL(fileURLWithPath: "Books",
+                               relativeTo: documentDirectoryURL).appendingPathExtension("plist")
+  let bundleId = "edu.umn.minitex.simplye.PDF"
+
+  init() {
+    // copy the plist from the bundle to the user's documents directory,
+    // so that it can be edited
+    let booksPlistPathInBundle: URL =
+      (Bundle(identifier: bundleId)?.url(forResource: "Books", withExtension: "plist")!)!
+    if !FileManager.default.fileExists(atPath: booksPlistURL.path) {
+      do {
+        try FileManager.default.copyItem(at: booksPlistPathInBundle, to: booksPlistURL)
+      } catch {
+        print(error)
+      }
+    }
+
+    // pull contents of Books plist file into internal Book array
+    do {
+      let data = try Data(contentsOf: booksPlistURL)
+      let decoder = PropertyListDecoder()
+      books = try decoder.decode([Book].self, from: data)
+      #if DEBUG
+        print(books ?? "no books value")
+      #endif
+
+      book = books![0]
+    } catch {
+      print(error)
+    }
+  }
+
+  // you DON'T want these lazy loaded so we can reload the data
+  // when the Books.plist changes
+  var lastPageRead: UInt {
+    var lastPageRead: UInt = 0
+    lastPageRead = (book?.lastPageRead)!
+    return lastPageRead
+  }
+
+  var bookmarkPages: [UInt] {
+    var bookmarkPages: [UInt] = []
+    bookmarkPages = (book?.bookmarks)!
+    return bookmarkPages
+  }
+
+  var annotationsData: [Data] {
+    var annotationsData: [Data] = []
+    annotationsData = (book?.annotations)!
+    return annotationsData
+  }
+
+  var documentURL: URL {
+    let documentName = book?.title
+    let fileURL = Bundle(identifier: bundleId)?.url(forResource: documentName, withExtension: "pdf")
     return fileURL!
   }
-}
+
+  // MockPDFViewControllerDelegateDelegate
+  func persistLastPageRead(page: Int) {
+    print("in MockData: persistLastPageRead")
+    book?.lastPageRead = UInt(page)
+    let encoder = PropertyListEncoder()
+    encoder.outputFormat = .xml
+
+    // save changes to books array to the Books.plist file
+    do {
+      let data = try encoder.encode(books)
+      try data.write(to: booksPlistURL, options: .atomic)
+    } catch {
+      print(error)
+    }
+  }
+
+  func persistBookmarks(pageNumbers: [UInt]) {
+    print("in MockData: persistBookmarks")
+  }
+
+  func persistAnnotations(annotationsData: [Data]) {
+    print("in MockData: persistAnnotations")
+  }}
--- PDFTests/PDFTests.swift
+++ PDFTests/PDFTests.swift
@@ -12,22 +12,9 @@ import XCTest
 class PDFTests: XCTestCase {
   // swiftlint:disable trailing_whitespace
 
-  // how to test bookmarks?
-  // in order to test BookmarkProvider and AnnotationProvider, must create a PDFViewControllerDelegate
-  var pdfViewControllerDelegate: PDFViewControllerDelegate? = nil
-
-  var documentURL: URL? = nil
-  var lastPageRead: UInt? = nil
-  var bookmarkPages: [UInt]? = nil
-  var annotationsData: [Data]? = nil
-
-  // can we test lastPageRead first? and how?
-  // create a PDFViewController, and then test that the pageIndex is indeed same as lastPageRead
-
   override func setUp() {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    setupTestData()
   }
   
   override func tearDown() {
@@ -35,34 +22,87 @@ class PDFTests: XCTestCase {
     super.tearDown()
   }
 
-  func setupTestData() {
-    // this is where we select the test data we're going to use
-    // ie what the last page read should be, bookmarks, annotations, etc.
-    documentURL = MockData.documentURL()
-    lastPageRead = MockData.lastPageRead
-    bookmarkPages = MockData.bookmarkPages
-    annotationsData = MockData.annotationsData
-    pdfViewControllerDelegate = MockPDFViewControllerDelegate()
+  func testDefaultMockData() {
+    checkMockDataLastPageRead()
+    checkMockDataBookmarkPages()
+    checkMockDataAnnotations()
   }
 
+  func checkMockDataLastPageRead() {
+    let mockData = MockData()
+    XCTAssert(mockData.lastPageRead == 0, "last page read is wrong")
+  }
 
-  func testLastPageRead() {
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    let pdfViewController = PDFViewController.init(documentURL: documentURL!,
-                                                   openToPage: lastPageRead!,
-                                                   bookmarks: bookmarkPages!,
-                                                   annotations: annotationsData!,
+  func checkMockDataBookmarkPages() {
+    let mockData = MockData()
+    XCTAssert(mockData.bookmarkPages == [], "bookmark pages are wrong")
+  }
+
+  func checkMockDataAnnotations() {
+    let mockData = MockData()
+    //XCTAssert(mockData.annotationsData == [Data(bytes: [8,9,10])], "annotations data is wrong")
+    XCTAssert(mockData.annotationsData == [], "annotations data is wrong")
+  }
+
+  func testImportLastPageRead() {
+    let mockData = MockData()
+    let pdfViewControllerDelegate = MockPDFViewControllerDelegate(mockPDFViewControllerDelegateDelegate: mockData)
+    let pdfViewController: PDFViewController? = PDFViewController.init(documentURL: mockData.documentURL,
+                                                                       openToPage: mockData.lastPageRead,
+                                                                       bookmarks: mockData.bookmarkPages,
+                                                                       annotations: mockData.annotationsData,
+                                                                       PSPDFKitLicense: MockAPIKeys.PDFLicenseKey,
+                                                                       delegate: pdfViewControllerDelegate)
+    XCTAssert(pdfViewController?.pageIndex == 0, "last page read is not correct")
+  }
+
+  // we are simulating opening the PDF view controller, changing the page number
+  // and then opening a new PDF view controller to see if the new page number is imported
+  // correctly between new versions of the PDF view controller (simulate opening, closing, and
+  // re-opening the app)
+  func testImportChangedLastPageRead() {
+    let mockData: MockData? = MockData()
+    let pdfViewControllerDelegate: MockPDFViewControllerDelegate = MockPDFViewControllerDelegate(mockPDFViewControllerDelegateDelegate: mockData!)
+    var pdfViewController: PDFViewController? = PDFViewController.init(documentURL: (mockData?.documentURL)!,
+                                                                       openToPage: (mockData?.lastPageRead)!,
+                                                                       bookmarks: (mockData?.bookmarkPages)!,
+                                                                       annotations: (mockData?.annotationsData)!,
                                                    PSPDFKitLicense: MockAPIKeys.PDFLicenseKey,
                                                    delegate: pdfViewControllerDelegate)
-    XCTAssert(pdfViewController.pageIndex == 0, "last page read is not correct")
+    XCTAssert(pdfViewController?.pageIndex == mockData?.lastPageRead, "last page read is not correct")
+
+    let newPage = 12
+    // Note: calling the PDFViewController's willbeingDisplaying (page turning method) directly doesn't work
+    // so we're calling the delegate method that saves off the page number directly
+    pdfViewControllerDelegate.userDidNavigate(page: newPage)
+    pdfViewController = nil
+    pdfViewController = PDFViewController.init(documentURL: (mockData?.documentURL)!,
+                                               openToPage: (mockData?.lastPageRead)!,
+                                               bookmarks: (mockData?.bookmarkPages)!,
+                                               annotations: (mockData?.annotationsData)!,
+                                               PSPDFKitLicense: MockAPIKeys.PDFLicenseKey,
+                                               delegate: pdfViewControllerDelegate)
+    XCTAssert(pdfViewController?.pageIndex == UInt(12), "last page read is not correct")
   }
 
+  func testInvalidLastPageRead() {
 
+  }
+
+  func testBookmarks() {
+
+  }
+
+  func testAnnotations() {
+    
+  }
+/*
   func testPerformanceExample() {
     // This is an example of a performance test case.
     self.measure {
       // Put the code you want to measure the time of here.
     }
   }
+ */
 
 }
