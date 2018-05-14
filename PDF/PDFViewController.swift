d395ffabd8cbd949cfe618a1a8d2620cf61320e1
Vui Nguyen <vui_engineer@yahoo.com>
2018-05-14 14:57:34 -0600

    remove deprecated code to save off/load annotations as Data, part 2

--- PDF/PDFViewController.swift
+++ PDF/PDFViewController.swift
@@ -12,38 +12,7 @@ import MinitexPDFProtocols
 
 public final class PDFViewController: PSPDFViewController {
 
-  //weak var pdfModuleDelegate: PDFViewControllerDelegate?
   weak var pdfModuleDelegate: MinitexPDFViewControllerDelegate?
-
-  // This initializer is now deprecated with passing annotations as MinitexPDFAnnotation objects
-  /*
-  public init(documentURL: URL, openToPage page: UInt = 0, bookmarks pages: [UInt] = [],
-              annotations annotationsData: [Data] = [], PSPDFKitLicense: String, delegate: PDFViewControllerDelegate?) {
-
-    PSPDFKit.setLicenseKey(PSPDFKitLicense)
-    let document = PSPDFDocument(url: documentURL)
-
-    document.annotationSaveMode = PSPDFAnnotationSaveMode.externalFile
-    document.didCreateDocumentProviderBlock = { (documentProvider: PSPDFDocumentProvider) -> Void in
-    documentProvider.annotationManager.annotationProviders = [PDFAnnotationProvider(annotationObjects: annotationsData,
-                                                                                    documentProvider: documentProvider,
-                                                                                    pdfModuleDelegate: delegate!)]
-    }
-
-    document.bookmarkManager?.provider = [PDFBookmarkProvider(pages: pages, pdfModuleDelegate: delegate!)]
-
-    let configuration = PSPDFConfiguration { builder in
-      builder.searchResultZoomScale = 1
-      builder.backgroundColor = UIColor.lightGray
-    }
-
-    super.init(document: document, configuration: configuration)
-    self.delegate = self
-
-    self.pdfModuleDelegate = delegate
-    self.pageIndex = page
-  }
- */
 
   public init(documentURL: URL, openToPage page: UInt = 0, bookmarks pages: [UInt] = [],
               annotations annotationObjects: [MinitexPDFAnnotation] = [],
